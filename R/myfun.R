d68b7165c96e4d06be357e27f2bc83b37241ad03
parksw3 <parksw3@mcmaster.ca>
2017-06-05 13:39:47 -0400

    package

---
+++ R/myfun.R
@@ -0,0 +1,31 @@
+##' my function
+##' @param x a numeric vector
+##' @param y a numeric vector
+##' @export
+myfun <- function(x, y) x + y
+
+##' my derivative function
+##' @param x a numeric vector
+##' @param y a numeric vector
+##' @export
+mydfun <- function(x, y) NULL
+
+##' Deriv2
+##' @param expr expression
+##' @param var variable
+##' @importFrom Deriv Deriv
+##' @importFrom Deriv drule
+##' @export
+##' @examples
+##' myderiv
+##' Deriv(myfun(x, y), c("x", "y"))
+##' Deriv2(myfun(x, y), c("x", "y"))
+Deriv2 <- function(expr, var) {
+    Deriv(expr, var)
+}
+
+drule[["myfun"]] <- alist(x=mydfun(x, x), y=mydfun(y, y))
+
+##' my derivative object
+##' @export
+myderiv <- Deriv(myfun(x, y), c("x", "y"))
